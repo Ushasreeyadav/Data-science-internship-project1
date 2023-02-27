fec6dd582c9bfbe6b380cbf6e8bbd2f370b91b36
Kundanapally Usha sree yadav <118263361+Ushasreeyadav@users.noreply.github.com>
2023-02-27 16:22:43 +0530

    Add files via upload

---
+++ app_1.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.header('Hello World and welcome to Internship Feb 2023')
---
+++ app_2.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title(":green[My First Data app] :sunglasses:")
+
+st.header("First task from the Internship at :red[Innomatics Reasearch Labs]")
+
+st.subheader("Starting at  Feb 2023 ")
+
+CODE = '''print("HELLO INNOMATICS PEOPLE WELCOME TO  DATA SCIENCE INTERNSHIP FEB-2023")'''
+
+st.code(CODE , language='python')
